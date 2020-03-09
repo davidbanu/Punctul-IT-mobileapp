efb3e6d3868473f947d02f439909831a3ad1b943
davidbanu <davidbanu@ymail.com>
2020-03-09 15:21:50 +0200

    Adding menu icon HomePage

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import  {StyleSheet, View, TextInput, TouchableOpacity, Text, StatusBar, Image} from 'react-native';
+import  {StyleSheet, View, TouchableOpacity, Text, StatusBar, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 import userData from '../assets/exampleUserData.json';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 function HomeComponent() {
     const navigation = useNavigation();
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" />
-            <Text style={styles.title}>Bună, {userData.username}!</Text>
+            <Icon name="ios-menu" size={40} style={styles.menu} color="#FFF" />
+            <Text style={styles.title}>Bună, {userData.firstName}!</Text>
             <View style={styles.avatarContainer}> 
                 <Image style={styles.avatar} source={require('../assets/avatar.jpg')}/>
             </View>
@@ -29,8 +31,13 @@ function HomeComponent() {
 }
 
 const styles = StyleSheet.create({
+    menu: {
+        marginLeft: 10,
+        marginTop: 10
+    }, 
+
     container: {
-        padding: 20,
+        padding: 10,
         marginBottom: 20
     },
 
@@ -53,7 +60,6 @@ const styles = StyleSheet.create({
 
     title: {
         flexGrow: 1,
-        marginTop: 20,
         padding: 20,
         textAlign: 'center',
         color: '#FFF',
